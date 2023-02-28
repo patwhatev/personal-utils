5474325d23b4792624376c38f1f623ede12ec67b
Patrick Everman <patrick.everman@bouncex.com>
2023-02-28 15:24:10 -0500

    hmm

--- yt-wav.sh
+++ yt-wav.sh
@@ -1,5 +1,5 @@
 # create video files from an image and song for yt 
-ffmpeg -loop 1 -i ima.jpg -i audio.wav -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest out.mp4
+ffmpeg -loop 1 -i content/sponge.jpg -i content/drive.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest drive.mp4
 
 # CONVERT SOME AUDIO FILES
 # shopt -s globstar
